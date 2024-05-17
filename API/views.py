af77ffd84101a3fcabf91a6f787d3d4fb1302c94
pasindu vishmika <56432141+Pasindu-Vishmika@users.noreply.github.com>
2024-05-18 02:49:05 +0530

    Register Book model in admin site and add new API endpoints

--- API/views.py
+++ API/views.py
@@ -10,3 +10,42 @@ def getRoutes(request):
         'book': '/book/<str:pk>/',
     }
     return Response(routes)
+
+def addNewBook(request):
+    pass
+
+def getBooks(request):
+    pass
+
+def searchBook(request , pk):
+    pass
+
+def deleteBook(request , pk):
+    pass
+
+def updateBook(request, pk ):
+    pass
+
+def addNewMember(request):
+    pass
+
+def searchMember(request ,pk):
+    pass
+
+def deleteMember(request , pk ):
+    pass
+
+def updateMember(request , pk):
+    pass
+
+def issueBook(request , pk ):
+    pass
+
+def returnBook(request , pk ):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
